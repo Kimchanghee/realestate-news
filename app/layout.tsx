ed3dadf42bb257c10b586fdc9b9a7993130a5e67
Kimchanghee <9594198+Kimchanghee@users.noreply.github.com>
2026-06-11 02:09:47 +0900

    Switch KoreaRealEstateNews GA4 tag to site property

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3LYD80FR0D" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y4PS20L8H3" />
         <script
           dangerouslySetInnerHTML={{
-            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-3LYD80FR0D',{page_path:window.location.pathname});",
+            __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-Y4PS20L8H3',{page_path:window.location.pathname});",
           }}
         />
       </head>
